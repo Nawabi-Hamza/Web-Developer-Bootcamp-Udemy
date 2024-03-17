57c0f913a3fd53670396ca7d5b0ee69b971ba460
Nawabi-Hamza <hamza.nawabi119@gmail.com>
2024-03-17 10:23:18 +0430

    section 14 finished let const practiced

---
+++ Section-14/README.md
@@ -0,0 +1,6 @@
+<h1>What is javascript ?</h1>
+<p>
+    html - none
+    css - adjective
+    javascript - verbs 
+</p>
---
+++ Section-14/first.js
@@ -0,0 +1,25 @@
+console.log( 2 + 8 ) // Addation
+console.log( 90 - 1 ) // Subtraction
+console.log( 3 * 24 ) // Multiplaction
+console.log( 400 / 25 ) // Division
+console.log( 3 + 1 * 6  )
+console.log( (3 + 1) * 6  ) // Order in math calculation
+console.log( 9 % 2 ) // Remindar
+console.log( 9 ** 2 ) // Exponentiaion
+console.log( typeof 2.3323)
+console.log( 0/0 ) // Nan (Not a Number)
+console.log( typeof NaN ) // type of NaN = Number
+
+// updating a variable 
+let numb = 34 ;
+console.log(numb);
+
+numb = numb + 10;
+console.log(numb)
+
+numb = numb + 10;
+console.log(numb)
+
+
+const a = 234;
+// a = 12 this is get error 
