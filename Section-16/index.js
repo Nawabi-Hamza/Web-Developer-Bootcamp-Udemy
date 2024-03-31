f08febfbc7bc0090f2a9d14ec790e4aaa0e98dfd
Nawabi-Hamza <hamza.nawabi119@gmail.com>
2024-03-31 11:37:53 +0430

    selction 16 finished

---
+++ Section-16/index.js
@@ -0,0 +1,21 @@
+function isEven(num){
+    if(num % 2 == 0){
+        console.log("even")
+    }
+}
+
+
+isEven(14)
+// const a = prompt("your name")
+if(true && true){
+    console.log("&& and operator")
+}
+
+if(false || true){
+    console.log("|| or oprator")
+}
+
+if(!null){
+    console.log("! not oprator")
+}
+
