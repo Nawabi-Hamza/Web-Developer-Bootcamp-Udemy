365078c9f645fb96c87d788aa304215040b79b82
Nawabi-Hamza <hamza.nawabi119@gmail.com>
2024-06-03 13:52:29 +0430

    in this section all about terminal about folders files delete and more

---
+++ Section-30/app.js
@@ -0,0 +1,18 @@
+/**
+ *   TERMINAL
+ *      a text-base  interface to your computer .
+ *      originally a physical object but now we use softaw=re terminals.
+ *    SHELL
+ *      the program running on the terminal
+ *      (TERMINAL :the ATM is the terminal)
+ *      (SHELL: the software running on the ATM is the shell)
+ *    BASH
+ *      one of the most popular shells (AND THE DEFAULT ON A MAC)
+ *      Z shell is another for mac
+ *   
+ * 
+ *      ls , pwd , cd , md ,mkdir , touch , --help , Relative / Absoulute path
+ *      rm , rmdir ,ls -l , rm -rf folder_name_which_has_file_inside 
+ *          
+ * 
+ */
