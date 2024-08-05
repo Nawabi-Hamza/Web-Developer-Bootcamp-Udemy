6ce4f33d47d7499c29acb2b7610012b40dd5ec7a
Nawabi-Hamza <hamza.nawabi119@gmail.com>
2024-08-05 14:32:37 +0430

    section 35 have finished it include RESTful CRUD with using EJS and HTTP Verbs

--- Section-35/app.js
+++ Section-35/app.js
@@ -2,49 +2,105 @@ console.log("server side")
 const express = require("express")
 const app = express()
 const path = require("path")
-
-
+const { v4:uuid } = require("uuid")
+const methodOverride = require("method-override")
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.set('views',path.join(__dirname,'views'))
 app.set("view engine" , 'ejs')
+app.use(methodOverride('_method'))
 
-const comments = [ 
+
+let comments = [ 
     {
+        id:uuid(),
         username:'Todd',
-        comment:'Lol tha t so funny'
+        comment:'Lol that so funny'
     },
     {
+        id:uuid(),
         username:'Joie',
         comment:'so cute'
     },
     {
+        id:uuid(),
         username:'Mike',
         comment:'fantasy'
     },
     {
+        id:uuid(),
         username:'Teddy',
         comment:'mmm that so crazy'
     }
 ]
 
 
-
+// show all comments ejs file
 app.get('/comments',(req,res)=>{
     res.render('comments/index',{comments})
 })
-
+// show new comment forms ejs file
 app.get('/comments/new',(req,res)=>{
     res.render('comments/new')
 })
-
+// create new comment
 app.post('/comments',(req,res)=>{
     const {username,comment} = req.body
-    comments.push({ username,comment })
+    comments.push({ username,comment,id:uuid() })
     res.redirect('/comments')
-    res.send("it work")
+    // res.send("it work")
 })
+// get single comment
+app.get("/comments/:id",(req,res)=>{
+    const { id } = req.params
+    const comment = comments.find( comment => comment.id === id ) 
+    res.render('comments/show',{comment})
+})
+// update single comment ejs file
+/**
+ *  for check update first go details copy url then open post man paste the url set method path in body in x-www-form-urlencoded 
+ *  set key comment and set a value the send after that go to index page then refresh page you can see the changes
+ */
+app.patch('/comments/:id',(req,res)=>{
+    const { id } = req.params
+    const newCommentText = req.body.comment
+    // console.log(newCommentText)
+    const foundComment = comments.find( c => c.id === id)
+    foundComment.comment = newCommentText
+    res.redirect('/comments')
+})
+// show edit page show form it does not need to use postman like above method 
+app.get('/comments/:id/edit',(req,res)=>{
+    const { id } = req.params
+    const comment = comments.find( c => c.id === id)
+    res.render('comments/edit',{comment})
+})
+// destroy a comment
+app.delete('/comments/:id',(req,res)=>{
+    const { id } = req.params
+    comments = comments.filter( c => c.id !== id)
+    res.redirect('/comments')
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get("/tacos",(req,res)=>{
     res.send("GET /tacos respons")
