16e6fddeb7962c68fb08ad49fabc4805ca2b07c4
Nawabi-Hamza <hamza.nawabi119@gmail.com>
2024-08-12 16:14:28 +0430

    section 39 finished

---
+++ Section-39/seeds/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const path = require("path")
+const mongoose = require("mongoose")
+const Campground = require("../models/campground")
+const cities = require('./cities')
+const { places,descriptors } = require('./seedHelpers')
+
+const app = express()
+
+
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'views'))
+
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp")
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,"connection error:"))
+db.once("open",()=>{
+    console.log("connected to db")
+})
+
+const sample = (array)=> array[Math.floor(Math.random() * array.length)]
+const seedDB = async()=>{
+    await Campground.deleteMany({});
+    for(let i=0; i<50; i++){
+        const random1000 = Math.floor(Math.random()*1000);
+        const camp = new Campground({
+            location:`${cities[random1000].city}, ${cities[random1000].state}`,
+            title:`${sample(descriptors) } ${sample(places)}`
+        })
+        await camp.save()
+    }
+    Campground.find()
+    .then(data=>console.log(data))
+    
+}
+
+seedDB()
+// .then(()=>{
+//     mongoose.connection.close()
+// })
