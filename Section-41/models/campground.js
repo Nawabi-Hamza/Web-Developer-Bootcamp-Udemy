ab9678dfb9b740a854bf879dbdf174f68833330c
Nawabi-Hamza <hamza.nawabi119@gmail.com>
2024-08-13 11:08:43 +0430

    section 41 finished worked ejs-mate for making component and add bootstrap style to compground crud

---
+++ Section-41/models/campground.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title:String,
+    image:String,
+    price:Number,
+    description:String,
+    location:String
+},{ timestamps:true })
+
+
+module.exports = mongoose.model('Campground',CampgroundSchema)
